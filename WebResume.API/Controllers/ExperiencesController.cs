a45fa7dbbc1e1b4cf1661f610b1ec93d49256122
S@ish <satishsuthar@live.com>
2023-01-18 18:07:59 +1100

    Stpe-2 route at controller level and add new get by id method.

--- WebResume.API/Controllers/ExperiencesController.cs
+++ WebResume.API/Controllers/ExperiencesController.cs
@@ -3,12 +3,19 @@
 namespace WebResume.API.Controllers
 {
     [ApiController]
+    [Route("api/expereiences")]
     public class ExperiencesController : ControllerBase
     {
-        [HttpGet("api/expereiences")]
+        [HttpGet]
         public JsonResult GetExperiences()
         {
             return new JsonResult(ExperienceDataStore.Instance.Experiences);
+        }
+
+        [HttpGet("{id}")]
+        public JsonResult GetExperienceById(Guid id) 
+        {
+            return new JsonResult(ExperienceDataStore.Instance.Experiences.FirstOrDefault(x => x.ExperienceId == id));
         }
     }
 }
